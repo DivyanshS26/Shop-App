cbbb16179d74b8dc5874c121007f7cf9222eac14
Divyansh <divyanshsahu25@gmail.com>
2021-10-04 00:22:52 +0530

    4th

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const seedDB = require('./seed');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
-const passport = require('passport')
+const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/users')
+const User = require('./models/users');
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('DB Connected'))
@@ -28,10 +28,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const sessionConfig = {
-    secret: 'wennedsomebettersecret',
+    secret: 'weneedsomebettersecret',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
 }
+
 
 app.use(session(sessionConfig));
 app.use(flash());
@@ -41,37 +42,42 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
 
 
-app.use((req, res, next) => {
 
+app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
     next();
+});
 
-})
+
 
 const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes')
-const cartRoutes = require('./routes/cartRouter');
+const authRoutes = require('./routes/authRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.get('/error', (req, res) => {
+    
     res.render('error');
 })
+
 
 app.use(productRoutes);
 app.use(authRoutes);
 app.use(cartRoutes);
+
 
 app.listen(process.env.PORT || 2323, (req, res) => {
     console.log('server running at port 2323');
